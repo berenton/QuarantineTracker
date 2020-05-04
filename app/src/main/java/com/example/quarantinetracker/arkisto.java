e38d6f30b8af819fc6680d56e204c86339d33452
berenton <56107579+berenton@users.noreply.github.com>
2020-05-04 12:28:13 +0300

    Lisätty arkisto

--- app/src/main/java/com/example/quarantinetracker/arkisto.java
+++ app/src/main/java/com/example/quarantinetracker/arkisto.java
@@ -1,64 +1,68 @@
 package com.example.quarantinetracker;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.DocumentsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.quarantinetracker.ui.DatabaseHelper;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link arkisto#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class arkisto extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView textField;
+    DatabaseHelper myDb;
+    Button refreshButton;
 
     public arkisto() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment arkisto.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static arkisto newInstance(String param1, String param2) {
-        arkisto fragment = new arkisto();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_arkisto, container, false);
+        myDb = new DatabaseHelper(getContext());
+        final View root = inflater.inflate(R.layout.fragment_arkisto, container, false);
+        textField = root.findViewById(R.id.textField);
+        textField.setText(getReports());
+        refreshButton = root.findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getReports();
+            }
+        });
+        return root;
+    }
+
+    public String getReports(){
+        Cursor res = myDb.getRaportData();
+        if(res.getCount() == 0){
+            return("No reports found");
+        }
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()){
+            buffer.append("Id : "+res.getInt(0)+ "\n");
+            buffer.append("Year : "+res.getInt(1)+ "\n");
+            buffer.append("Month : "+res.getInt(2)+ "\n");
+            buffer.append("Day : "+res.getInt(3)+ "\n");
+            buffer.append("Hour : "+res.getInt(4)+ "\n");
+            buffer.append("Minute : "+res.getInt(5)+ "\n");
+            buffer.append("Title : "+res.getString(6)+ "\n");
+            buffer.append("Location : "+res.getString(7)+ "\n");
+            buffer.append("Assessment : "+res.getString(8)+ "\n");
+            buffer.append("People : "+res.getString(9)+ "\n");
+            buffer.append("Misc : "+res.getString(10)+ "\n\n");
+        }
+        return(buffer.toString());
     }
 }
