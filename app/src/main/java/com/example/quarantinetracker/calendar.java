7a50404475ea77473b7a53c48c253af039f16c51
Mika-git <mika.aberg1@hotmail.com>
2020-05-04 15:39:47 +0300

    kalenteri data

--- app/src/main/java/com/example/quarantinetracker/calendar.java
+++ app/src/main/java/com/example/quarantinetracker/calendar.java
@@ -1,26 +1,23 @@
 package com.example.quarantinetracker;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quarantinetracker.ui.DatabaseHelper;
+import com.example.quarantinetracker.ui.slideshow.SlideshowViewModel;
 
 
 /**
@@ -37,6 +34,8 @@ public class calendar extends Fragment {
     TextView text;
 
     private String date;
+    private SlideshowViewModel slideshowViewModel;
+    DatabaseHelper myDb;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -72,22 +71,67 @@ public class calendar extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
     //
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        myDb = new DatabaseHelper(getContext());
+        slideshowViewModel =
+                ViewModelProviders.of(this).get(SlideshowViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         calendarView = root.findViewById(R.id.calendarView);
-        text = root.findViewById(R.id.textView3);
-
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                date = dayOfMonth + "/" + (month +1) + "/" + year; // kuukausi alkaa nollasta, joten pitää lisätä kuukauteen yksi
+                date = dayOfMonth + "/" + (month + 1) + "/" + year; // Tammikuu alkaa nollasta, joten pitää lisätä kuukauteen yksi
                 text.setText(date);
             }
         });
+
+        text = root.findViewById(R.id.buttonData);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showData(v);
+            }
+        });
+
         return root;
     }
+
+    public void showData(View v){
+        Cursor res = myDb.getReportData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found.");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()){
+            buffer.append("Id : "+res.getInt(0)+ "\n");
+            buffer.append("Year : "+res.getInt(1)+ "\n");
+            buffer.append("Month : "+res.getInt(2)+ "\n");
+            buffer.append("Day : "+res.getInt(3)+ "\n");
+            buffer.append("Hour : "+res.getInt(4)+ "\n");
+            buffer.append("Minute : "+res.getInt(5)+ "\n");
+            buffer.append("Title : "+res.getString(6)+ "\n");
+            buffer.append("Location : "+res.getString(7)+ "\n");
+            buffer.append("Assessment : "+res.getString(8)+ "\n");
+            buffer.append("People : "+res.getString(9)+ "\n");
+            buffer.append("Misc : "+res.getString(10)+ "\n\n");
+        }
+        showMessage("Data: ", buffer.toString());
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
 }
+
+
+
